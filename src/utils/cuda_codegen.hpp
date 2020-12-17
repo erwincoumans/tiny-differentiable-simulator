17bde9664a3d52a62f506b375b13268a398814d0
Eric Heiden <eric-heiden@outlook.com>
2020-12-17 23:41:27 +0100

    Update CUDA codegen

--- src/utils/cuda_codegen.hpp
+++ src/utils/cuda_codegen.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <numeric>
+
 #include <cppad/cg.hpp>
 #include <cppad/cg/arithmetic.hpp>
 #include <cppad/cg/support/cppadcg_eigen.hpp>
@@ -572,51 +574,78 @@ template <typename Scalar,
 struct CudaFunction {
   using FunctionPtrT = kFunctionPtrT;
   std::string function_name;
-  CudaFunctionMetaData meta_data{};
+
+ protected:
+  CudaFunctionMetaData meta_data_{};
+  bool is_available_{false};
+
+  public:
+  bool is_available() const { return is_available_; };
 
   /**
    * Global input dimension.
    */
-  int global_input_dim() const { return meta_data.global_input_dim; }
+  int global_input_dim() const { return meta_data_.global_input_dim; }
   /**
    * Input dimension per thread.
    */
-  int local_input_dim() const { return meta_data.local_input_dim; }
+  int local_input_dim() const { return meta_data_.local_input_dim; }
   /**
    * Output dimension per thread.
    */
-  int output_dim() const { return meta_data.output_dim; }
+  int output_dim() const { return meta_data_.output_dim; }
   /**
    * Determines whether the output is accumulated over all threads.
    */
-  bool accumulated_output() const { return meta_data.accumulated_output; }
+  bool accumulated_output() const { return meta_data_.accumulated_output; }
 
   CudaFunction() = default;
   CudaFunction(const std::string& function_name, void* lib_handle)
       : function_name(function_name) {
-    fun_ = load_function<FunctionPtrT>(function_name, lib_handle);
-    auto meta_data_fun = load_function<MetaDataFunctionPtrT>(
-        function_name + "_meta", lib_handle);
-    meta_data = meta_data_fun();
-    allocate_ = load_function<AllocateFunctionPtrT>(function_name + "_allocate",
-                                                    lib_handle);
-    deallocate_ = load_function<DeallocateFunctionPtrT>(
-        function_name + "_deallocate", lib_handle);
-    send_global_fun_ = load_function<SendGlobalFunctionPtrT<Scalar>>(
-        function_name + "_send_global", lib_handle);
-    send_local_fun_ = load_function<SendLocalFunctionPtrT<Scalar>>(
-        function_name + "_send_local", lib_handle);
+    try {
+      fun_ = load_function<FunctionPtrT>(function_name, lib_handle);
+      is_available_ = true;
+    } catch (const std::runtime_error& ex) {
+      is_available_ = false;
+    }
+    if (is_available_) {
+      auto meta_data_fun = load_function<MetaDataFunctionPtrT>(
+          function_name + "_meta", lib_handle);
+      meta_data_ = meta_data_fun();
+      allocate_ = load_function<AllocateFunctionPtrT>(
+          function_name + "_allocate", lib_handle);
+      deallocate_ = load_function<DeallocateFunctionPtrT>(
+          function_name + "_deallocate", lib_handle);
+      send_global_fun_ = load_function<SendGlobalFunctionPtrT<Scalar>>(
+          function_name + "_send_global", lib_handle);
+      send_local_fun_ = load_function<SendLocalFunctionPtrT<Scalar>>(
+          function_name + "_send_local", lib_handle);
+    }
   }
 
   inline void allocate(int num_total_threads) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     allocate_(num_total_threads);
   }
 
-  inline void deallocate() const { deallocate_(); }
+  inline void deallocate() const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
+    deallocate_();
+  }
 
   inline bool operator()(int num_total_threads, int num_blocks,
                          int num_threads_per_block, Scalar* output,
                          const Scalar* input) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(fun_);
     bool status;
     status = send_global_input(input);
@@ -624,7 +653,7 @@ struct CudaFunction {
     if (!status) {
       return false;
     }
-    status = send_local_input(&(input[meta_data.global_input_dim]));
+    status = send_local_input(&(input[meta_data_.global_input_dim]));
     assert(status);
     if (!status) {
       return false;
@@ -635,6 +664,10 @@ struct CudaFunction {
 
   inline bool operator()(int num_total_threads, Scalar* output,
                          int num_threads_per_block = 32) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(fun_);
     int num_blocks = num_total_threads / num_threads_per_block;
     fun_(num_total_threads, num_blocks, num_threads_per_block, output);
@@ -645,6 +678,10 @@ struct CudaFunction {
                          const std::vector<std::vector<Scalar>>& local_inputs,
                          int num_threads_per_block = 32,
                          const std::vector<Scalar>& global_input = {}) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(fun_);
     bool status;
     status = send_local_input(local_inputs);
@@ -661,7 +698,7 @@ struct CudaFunction {
     }
 
     int num_total_threads = static_cast<int>(local_inputs.size());
-    Scalar* output = new Scalar[num_total_threads * meta_data.output_dim];
+    Scalar* output = new Scalar[num_total_threads * meta_data_.output_dim];
 
     int num_blocks = num_total_threads / num_threads_per_block;
 
@@ -670,7 +707,7 @@ struct CudaFunction {
 
     // assign thread-wise outputs
     std::size_t i = 0;
-    if (meta_data.accumulated_output) {
+    if (meta_data_.accumulated_output) {
       assert(thread_outputs->size() >= 1);
       for (; i < output_dim(); ++i) {
         (*thread_outputs)[0][i] = output[i];
@@ -691,14 +728,22 @@ struct CudaFunction {
 
   inline bool send_local_input(int num_total_threads,
                                const Scalar* input) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(send_local_fun_);
     return send_local_fun_(num_total_threads, input);
   }
   inline bool send_local_input(
       const std::vector<std::vector<Scalar>>& thread_inputs) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(send_local_fun_);
     if (thread_inputs.empty() || static_cast<int>(thread_inputs[0].size()) !=
-                                     meta_data.local_input_dim) {
+                                     meta_data_.local_input_dim) {
       assert(false);
       return false;
     }
@@ -716,20 +761,32 @@ struct CudaFunction {
     return status;
   }
   inline bool send_local_input(const std::vector<Scalar>& thread_inputs) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(send_local_fun_);
     auto num_total_threads =
-        static_cast<int>(thread_inputs.size() / meta_data.input_dim);
+        static_cast<int>(thread_inputs.size() / meta_data_.input_dim);
     return send_local_fun_(num_total_threads, thread_inputs.data());
   }
 
   inline bool send_global_input(const Scalar* input) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(send_global_fun_);
     return send_global_fun_(input);
   }
 
   inline bool send_global_input(const std::vector<Scalar>& input) const {
+    if (!is_available_) {
+      throw std::runtime_error("Function \"" + function_name +
+                               "\" is not available.");
+    }
     assert(send_global_fun_);
-    if (static_cast<int>(input.size()) != meta_data.global_input_dim) {
+    if (static_cast<int>(input.size()) != meta_data_.global_input_dim) {
       assert(false);
       return false;
     }
@@ -754,6 +811,7 @@ struct CudaModel {
 
  public:
   CudaFunction<Scalar> forward_zero;
+  CudaFunction<Scalar> sparse_jacobian;
 
 #if CPPAD_CG_SYSTEM_WIN
   CudaModel(const std::string& model_name) : model_name_(model_name) {
@@ -770,6 +828,8 @@ struct CudaModel {
     }
     forward_zero =
         CudaFunction<Scalar>(model_name + "_forward_zero", lib_handle_);
+    sparse_jacobian =
+        CudaFunction<Scalar>(model_name + "_jacobian", lib_handle_);
   }
 #else
   // loads the shared library
