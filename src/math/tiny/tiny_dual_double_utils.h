d2cd1a6a04c6f56b16809275c47b8ed80325f195
erwincoumans <erwincoumans@google.com>
2021-10-29 13:47:01 -0700

    Update tiny_dual_double_utils.h
    remove license block, this file doesn't have derived work.

--- src/math/tiny/tiny_dual_double_utils.h
+++ src/math/tiny/tiny_dual_double_utils.h
@@ -15,31 +15,6 @@
  */
 
 
-// this work contains parts derived from 
-// https://github.com/dtecta/motion-toolkit
-// MIT license
-
-// Copyright(c) 2006 Gino van den Bergen, DTECTA
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this softwareand associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-// 
-// The above copyright noticeand this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
-
 #ifndef _TINY_DUAL_DOUBLE_UTILS_H
 #define _TINY_DUAL_DOUBLE_UTILS_H
 
